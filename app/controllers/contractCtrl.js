2138d5af8815af1dd3c35cc870403e86e04b8a3d
lybstore <thelybstore@gmail.com>
2016-05-06 12:45:15 +0100

    json object upload

---
+++ app/controllers/contractCtrl.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var app = angular.module('auApp');
+
+app.service("contractService", function ($http, $q){
+    var deferred = $q.defer();
+    $http.get('app/data/contract.json').then(function (response){
+        deferred.resolve(response.data);
+    });
+
+    this.getcontract = function () {
+        return deferred.promise;
+    };
+})
+
+angular
+.module('auApp')
+.controller('contractCtrl', ['$scope', 'contractService', '$location', function($scope, contractService, $location) {
+
+  var promise = contractService.getcontract();
+  promise.then(function (data){
+      $scope.contract = data.contract;
+  });
+
+  // USERS OBJECTS
+
+
+
+}])
